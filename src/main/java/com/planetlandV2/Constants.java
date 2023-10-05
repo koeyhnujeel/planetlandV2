9d482800e07e1fbab23f4997a606472b76becd86
zunza <fksfks015@naver.com>
2023-10-06 00:39:25 +0900

    edit method getImgName

---
+++ src/main/java/com/planetlandV2/Constants.java
@@ -0,0 +1,5 @@
+package com.planetlandV2;
+
+public class Constants {
+	public static final String PATH = System.getProperty("user.dir") + "/src/main/resources/images/";
+}
--- src/main/java/com/planetlandV2/service/PlanetService.java
+++ src/main/java/com/planetlandV2/service/PlanetService.java
@@ -1,7 +1,11 @@
 package com.planetlandV2.service;
 
+import static com.planetlandV2.Constants.*;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -10,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.planetlandV2.exception.InvalidRequest;
 import com.planetlandV2.exception.PlanetNotFound;
-import com.planetlandV2.image.CreateFile;
 import com.planetlandV2.domain.Planet;
 import com.planetlandV2.repository.PlanetRepository;
 import com.planetlandV2.requset.PlanetCreate;
@@ -28,7 +31,7 @@ public class PlanetService {
 
 	public void create(PlanetCreate request, MultipartFile imgFile) throws IOException {
 
-		String imgName = CreateFile.getImgName(imgFile);
+		String imgName = getImgName(imgFile);
 
 		Planet planet = Planet.builder()
 			.planetName(request.getPlanetName())
@@ -37,7 +40,7 @@ public class PlanetService {
 			.satellite(request.getSatellite())
 			.planetStatus(request.getPlanetStatus())
 			.imgName(imgName)
-			.imgPath(CreateFile.PATH + imgName)
+			.imgPath(PATH + imgName)
 			.build();
 
 		planetRepository.save(planet);
@@ -65,8 +68,8 @@ public class PlanetService {
 			.orElseThrow(() -> new PlanetNotFound());
 
 		if (imgFile != null) {
-			String imgName = CreateFile.getImgName(imgFile);
-			String imgPath = CreateFile.PATH + imgName;
+			String imgName = getImgName(imgFile);
+			String imgPath = PATH + imgName;
 
 			planet.imgEdit(imgName, imgPath);
 		}
@@ -93,5 +96,15 @@ public class PlanetService {
 			throw new InvalidRequest("planetName", "이미 존재하는 행성입니다.");
 		}
 		return "사용 가능한 행성 이름입니다.";
+	}
+
+	private String getImgName(MultipartFile imgFile) throws IOException {
+		String originName = imgFile.getOriginalFilename();
+
+		UUID uuid = UUID.randomUUID();
+		String imgName = uuid + "_" + originName;
+
+		imgFile.transferTo(new File(PATH + imgName));
+		return imgName;
 	}
 }
