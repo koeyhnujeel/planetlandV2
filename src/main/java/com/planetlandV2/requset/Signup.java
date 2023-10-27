871645b4b12a3688389199b2f4f83e801fe2eab0
zunza <fksfks015@naver.com>
2023-10-27 20:55:52 +0900

    validation 어노테이션 추가

--- src/main/java/com/planetlandV2/requset/Signup.java
+++ src/main/java/com/planetlandV2/requset/Signup.java
@@ -1,6 +1,7 @@
 package com.planetlandV2.requset;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 import com.planetlandV2.domain.User;
 
@@ -13,9 +14,13 @@ import lombok.NoArgsConstructor;
 public class Signup {
 
 	@Email(message = "올바른 이메일 형식이 아닙니다.")
+	@NotBlank(message = "이메일을 입력해주세요.")
 	private String email;
 
+	@NotBlank(message = "비빌번호를 입력해주세요.")
 	private String password;
+
+	@NotBlank(message = "닉네임을 입력해주세요.")
 	private String nickname;
 
 	@Builder
@@ -30,6 +35,7 @@ public class Signup {
 			.email(this.email)
 			.password(encryptedPassword)
 			.nickname(this.nickname)
+			.balance(10000)
 			.build();
 	}
 }
