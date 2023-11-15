89f8ebd8ba071b151c31e16a640b4b78bedf2977
zunza <fksfks015@naver.com>
2023-11-15 22:28:04 +0900

    생성자

--- src/main/java/com/planetlandV2/requset/PlanetSell.java
+++ src/main/java/com/planetlandV2/requset/PlanetSell.java
@@ -3,6 +3,7 @@ package com.planetlandV2.requset;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,4 +16,9 @@ public class PlanetSell {
 	@Min(value = 1, message = "행성 최소 가격은 1원입니다.")
 	@NotNull(message = "판매 가격을 입력해주세요.")
 	private Integer sellPrice;
+
+	@Builder
+	public PlanetSell(Integer sellPrice) {
+		this.sellPrice = sellPrice;
+	}
 }
