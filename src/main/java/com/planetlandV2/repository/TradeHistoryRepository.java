7c83220cca645d0894864b5adc7227929e6c7b4f
zunza <fksfks015@naver.com>
2023-11-17 11:30:42 +0900

    findByUserId 메서드 추가

--- src/main/java/com/planetlandV2/repository/TradeHistoryRepository.java
+++ src/main/java/com/planetlandV2/repository/TradeHistoryRepository.java
@@ -1,5 +1,7 @@
 package com.planetlandV2.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.planetlandV2.domain.TradeHistory;
 
 @Repository
 public interface TradeHistoryRepository extends CrudRepository<TradeHistory, Long> {
+	List<TradeHistory> findByUserId(Long userId);
 }
