8f585a16ceffe48bd34e0e1370f4d2b586397a46
zunza <fksfks015@naver.com>
2024-01-22 16:51:07 +0900

    아이디, 이메일 중복 체크

--- src/main/java/com/planetlandV2/exception/ExistsEmailAndNicknameException.java
+++ src/main/java/com/planetlandV2/exception/ExistsEmailAndNicknameException.java
@@ -1,15 +1,15 @@
 package com.planetlandV2.exception;
 
-public class ExistsEmailException extends CustomException {
+public class ExistsEmailAndNicknameException extends CustomException {
 
-	private static final String MESSAGE = "이미 가입된 이메일입니다.";
+	private static final String MESSAGE = "이미 가입된 이메일과 사용중인 닉네임입니다.";
 
-	public ExistsEmailException() {
+	public ExistsEmailAndNicknameException() {
 		super(MESSAGE);
 	}
 
 	@Override
 	public int getStatusCode() {
-		return 400;
+		return 409;
 	}
 }
