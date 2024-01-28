f11d5b49ebb3c383319543c6dbdf920a9e71fcc8
zunza <fksfks015@naver.com>
2024-01-29 00:43:25 +0900

    security password encoder

--- src/main/java/com/planetlandV2/service/AuthService.java
+++ src/main/java/com/planetlandV2/service/AuthService.java
@@ -1,18 +1,12 @@
 package com.planetlandV2.service;
 
-import java.util.List;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.planetlandV2.crypto.PasswordEncoder;
 import com.planetlandV2.domain.User;
 import com.planetlandV2.exception.signup.ExistsEmailAndNicknameException;
 import com.planetlandV2.exception.signup.ExistsEmailException;
 import com.planetlandV2.exception.signup.ExistsNicknameException;
-import com.planetlandV2.exception.login.InvalidSignInInformation;
-import com.planetlandV2.exception.login.SessionNotFound;
-import com.planetlandV2.exception.UserNotFound;
 import com.planetlandV2.repository.UserRepository;
 import com.planetlandV2.request.Signup;
 
@@ -25,11 +19,11 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthService {
 
 	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder = new PasswordEncoder();
+	private final PasswordEncoder passwordEncoder;
 
 	public void signup(Signup signup) {
 		checkDuplicate(signup);
-		String encryptedPassword = passwordEncoder.encrpyt(signup.getPassword());
+		String encryptedPassword = passwordEncoder.encode(signup.getPassword());
 
 		User user = signup.toEntity(encryptedPassword);
 		userRepository.save(user);
