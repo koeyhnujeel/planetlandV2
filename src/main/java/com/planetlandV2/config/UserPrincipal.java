3f7a36cdd2a00a698ecfa2c255055b4490b32e34
zunza <fksfks015@naver.com>
2024-01-29 00:44:56 +0900

    UserPrincipal

--- src/main/java/com/planetlandV2/config/UserPrincipal.java
+++ src/main/java/com/planetlandV2/config/UserPrincipal.java
@@ -1,2 +1,20 @@
-package com.planetlandV2.config;public class UserPrincipal {
+package com.planetlandV2.config;
+
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class UserPrincipal extends User {
+
+	private final Long userId;
+
+	public UserPrincipal(com.planetlandV2.domain.User user) {
+		super(user.getEmail(), user.getPassword(), List.of(new SimpleGrantedAuthority("ADMIN")));
+		this.userId = user.getId();
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
 }
