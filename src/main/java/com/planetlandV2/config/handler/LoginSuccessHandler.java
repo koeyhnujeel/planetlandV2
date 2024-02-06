b5eaf5b1a9c919e3de21db087d74c89abfbb1f5e
zunza <fksfks015@naver.com>
2024-02-07 03:17:31 +0900

    로그인&로그아웃 핸들러

--- src/main/java/com/planetlandV2/config/handler/LoginSuccessHandler.java
+++ src/main/java/com/planetlandV2/config/handler/LoginSuccessHandler.java
@@ -7,16 +7,22 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.planetlandV2.config.UserPrincipal;
 import com.planetlandV2.constant.HttpStatusCode;
+import com.planetlandV2.response.LoginSuccessResponse;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	private final ObjectMapper objectMapper;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -25,8 +31,13 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 		UserPrincipal principal = (UserPrincipal)authentication.getPrincipal();
 		log.info("[인증성공] user={}", principal.getUsername());
 
+		LoginSuccessResponse loginResponse = LoginSuccessResponse.builder()
+			.nickname(principal.getNickname())
+			.build();
+
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 		response.setStatus(HttpStatusCode.OK);
+		objectMapper.writeValue(response.getWriter(), loginResponse);
 	}
 }
--- src/main/java/com/planetlandV2/config/handler/LogoutSuccessHandler.java
+++ src/main/java/com/planetlandV2/config/handler/LogoutSuccessHandler.java
@@ -1,2 +1,40 @@
-package com.planetlandV2.config.handler;public class LogoutSuccessHandler {
+package com.planetlandV2.config.handler;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.planetlandV2.config.UserPrincipal;
+import com.planetlandV2.constant.HttpStatusCode;
+import com.planetlandV2.response.LogoutSuccessResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LogoutSuccessHandler implements
+	org.springframework.security.web.authentication.logout.LogoutSuccessHandler {
+
+	private final ObjectMapper objectMapper;
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+		log.info("[로그아웃]");
+
+		LogoutSuccessResponse logoutResponse = LogoutSuccessResponse.builder()
+			.message("You have been logged out.")
+			.build();
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		response.setStatus(HttpStatusCode.OK);
+		objectMapper.writeValue(response.getWriter(), logoutResponse);
+	}
 }
