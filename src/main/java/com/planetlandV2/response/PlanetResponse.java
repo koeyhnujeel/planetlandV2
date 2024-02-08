cb315c56f0c04ef8485323cf0597d1e373dfb673
zunza <fksfks015@naver.com>
2024-02-08 22:49:02 +0900

    응답 필드 수정

--- src/main/java/com/planetlandV2/response/PlanetResponse.java
+++ src/main/java/com/planetlandV2/response/PlanetResponse.java
@@ -18,30 +18,13 @@ public class PlanetResponse {
 
 	private Integer price;
 
-	private Integer population;
-
-	private Integer satellite;
-
 	private PlanetStatus planetStatus;
 
-	private String owner;
-
-	private String imgName;
-
-	private String imgPath;
-
-
 	@Builder
-	public PlanetResponse(Long id, String planetName, Integer price, Integer population, Integer satellite,
-		PlanetStatus planetStatus, String owner, String imgName, String imgPath) {
+	public PlanetResponse(Long id, String planetName, Integer price, PlanetStatus planetStatus) {
 		this.id = id;
 		this.planetName = planetName;
 		this.price = price;
-		this.population = population;
-		this.satellite = satellite;
 		this.planetStatus = planetStatus;
-		this.owner = owner;
-		this.imgName = imgName;
-		this.imgPath = imgPath;
 	}
 }
