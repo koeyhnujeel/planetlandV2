8ac3158e512adb78e75310fac16c37e4605dd678
zunza <fksfks015@naver.com>
2024-02-08 22:51:42 +0900

    application/octet-stream' not supported 오류 수정을 위한

--- src/main/java/com/planetlandV2/converter/MultipartJackson2HttpMessageConverter.java
+++ src/main/java/com/planetlandV2/converter/MultipartJackson2HttpMessageConverter.java
@@ -1,2 +1,14 @@
-package com.planetlandV2.converter;public class MultipartJackson2HttpMessageConverter {
+package com.planetlandV2.converter;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+	protected MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+		super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+	}
 }
