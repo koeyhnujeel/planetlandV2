7aae228a9e2a66a66b93b9386253e3720d3a4213
zunza <fksfks015@naver.com>
2024-02-25 02:40:42 +0900

    :sparkles: 비관적 락 적용한 조회 메서드 추가

--- src/main/java/com/planetlandV2/repository/PlanetRepository.java
+++ src/main/java/com/planetlandV2/repository/PlanetRepository.java
@@ -1,12 +1,23 @@
 package com.planetlandV2.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.planetlandV2.domain.Planet;
+
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface PlanetRepository extends JpaRepository<Planet, Long>, PlanetRepositoryCustom {
 
 	boolean existsByPlanetName(String planetName);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query(value = "SELECT p FROM Planet p WHERE p.id = :id")
+	Optional<Planet> findByIdForBuy(@Param("id") Long planetId);
 }
