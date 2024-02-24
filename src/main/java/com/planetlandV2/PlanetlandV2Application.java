31807276d159dc8f9ba113de6c34e67f33c0a3e5
zunza <fksfks015@naver.com>
2024-02-25 02:08:55 +0900

    :sparkles: 스케쥴링 기능 추가

--- src/main/java/com/planetlandV2/PlanetlandV2Application.java
+++ src/main/java/com/planetlandV2/PlanetlandV2Application.java
@@ -2,7 +2,9 @@ package com.planetlandV2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PlanetlandV2Application {
 
