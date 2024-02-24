542d3785a9068180274708db5a432b0b6cfff791
zunza <fksfks015@naver.com>
2024-02-25 02:11:33 +0900

    :sparkles: 정적 리소스 제공 기능 추가

--- src/main/java/com/planetlandV2/config/WebMvcConfig.java
+++ src/main/java/com/planetlandV2/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.planetlandV2.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -16,5 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			.allowedOrigins("http://localhost:3000")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowCredentials(true);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+			.addResourceLocations("file:///Users/zunza/planetlandV2/src/main/resources/static/images/");
 	}
 }
