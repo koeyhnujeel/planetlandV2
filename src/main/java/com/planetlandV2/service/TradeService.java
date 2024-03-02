c849c6b27f0e5d646cc1d218f33320df0db8d126
zunza <fksfks015@naver.com>
2024-03-02 15:21:18 +0900

    :art: 코드 구조 개선

--- src/main/java/com/planetlandV2/service/TradeService.java
+++ src/main/java/com/planetlandV2/service/TradeService.java
@@ -5,19 +5,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.planetlandV2.Enum.PlanetStatus;
+import com.planetlandV2.Manager.TradeManager;
+import com.planetlandV2.Manager.TransactionManager;
+import com.planetlandV2.Reader.PlanetReader;
+import com.planetlandV2.Reader.UserReader;
 import com.planetlandV2.config.UserPrincipal;
 import com.planetlandV2.domain.Planet;
-import com.planetlandV2.domain.Transaction;
 import com.planetlandV2.domain.User;
 import com.planetlandV2.exception.trade.AlreadyOnSaleException;
 import com.planetlandV2.exception.trade.NotEnoughBalanceException;
 import com.planetlandV2.exception.trade.NotForSaleException;
 import com.planetlandV2.exception.trade.NotOwnerException;
-import com.planetlandV2.exception.planet.PlanetNotFound;
-import com.planetlandV2.exception.UserNotFound;
-import com.planetlandV2.repository.PlanetRepository;
-import com.planetlandV2.repository.TransactionRepository;
-import com.planetlandV2.repository.UserRepository;
 import com.planetlandV2.request.PlanetSell;
 
 import lombok.RequiredArgsConstructor;
@@ -26,68 +24,68 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TradeService {
 
-	private final UserRepository userRepository;
-	private final PlanetRepository planetRepository;
-	private final TransactionRepository transactionRepository;
+	private final UserReader userReader;
+	private final PlanetReader planetReader;
+	private final TradeManager tradeManager;
+	private final TransactionManager transactionManager;
 
 	@Transactional
-	public void sell(@AuthenticationPrincipal UserPrincipal userPrincipal, Long planetId, PlanetSell planetSell) {
-		User user = userRepository.findById(userPrincipal.getUserId())
-			.orElseThrow(UserNotFound::new);
+	public void sellPlanet(@AuthenticationPrincipal UserPrincipal userPrincipal, Long planetId, PlanetSell planetSell) {
 
-		Planet planet = planetRepository.findById(planetId)
-			.orElseThrow(PlanetNotFound::new);
-		if (planet.getPlanetStatus().equals(PlanetStatus.FORSALE)) throw new AlreadyOnSaleException();
+		User user = userReader.read(userPrincipal.getUserId());
+		Planet planet = planetReader.read(planetId);
 
+		checkNotForSale(planet.getPlanetStatus());
 		checkOwner(user, planet);
-		planet.editPriceAndStatus(planetSell);
+		tradeManager.sell(planet, planetSell);
 	}
 
 	@Transactional
-	public void buy(@AuthenticationPrincipal UserPrincipal userPrincipal, Long planetId) {
-		Planet planet = planetRepository.findByIdForBuy(planetId)
-			.orElseThrow(PlanetNotFound::new);
-		if (planet.getPlanetStatus().equals(PlanetStatus.NOTFORSALE)) throw new NotForSaleException();
+	public void buyPlanet(@AuthenticationPrincipal UserPrincipal userPrincipal, Long planetId) {
 
-		User buyer = userRepository.findById(userPrincipal.getUserId())
-			.orElseThrow(UserNotFound::new);
+		Planet planet = planetReader.readForBuy(planetId);
+		checkForSale(planet.getPlanetStatus());
 
-		User seller = userRepository.findByNickname(planet.getOwner())
-			.orElseThrow(UserNotFound::new);
+		User buyer = userReader.read(userPrincipal.getUserId());
+		checkBalance(buyer.getBalance(), planet.getPrice());
 
-		if (buyer.getBalance() < planet.getPrice()) {
-			throw new NotEnoughBalanceException();
-		}
+		User seller = userReader.read(planet.getOwner());
 
-		buyer.addPlanetAndBalanceDecrease(planet);
-		seller.deletePlanetAndBalanceIncrease(planet);
-		planet.changeOwnerAndStatus(buyer);
-
-		Transaction transaction = Transaction.builder()
-			.buyer(buyer)
-			.seller(seller)
-			.planet(planet)
-			.price(planet.getPrice())
-			.savedPlanetName(planet.getPlanetName())
-			.build();
-		transactionRepository.save(transaction);
+		tradeManager.deal(buyer, seller, planet);
+		transactionManager.create(buyer, seller, planet);
 	}
 
 	@Transactional
-	public void cancel(@AuthenticationPrincipal UserPrincipal userPrincipal, Long planetId) {
-		User user = userRepository.findById(userPrincipal.getUserId())
-			.orElseThrow(UserNotFound::new);
+	public void sellCancelPlanet(@AuthenticationPrincipal UserPrincipal userPrincipal, Long planetId) {
 
-		Planet planet = planetRepository.findById(planetId)
-			.orElseThrow(UserNotFound::new);
+		User user = userReader.read(userPrincipal.getUserId());
+		Planet planet = planetReader.read(planetId);
 
 		checkOwner(user, planet);
-		planet.changeStatus(PlanetStatus.NOTFORSALE);
+		tradeManager.cancel(planet);
 	}
 
 	private void checkOwner(User user, Planet planet) {
 		if (!user.getNickname().equals(planet.getOwner())) {
 			throw new NotOwnerException();
+		}
+	}
+
+	private void checkForSale(PlanetStatus planetStatus) {
+		if (planetStatus.equals(PlanetStatus.NOTFORSALE)) {
+			throw new NotForSaleException();
+		}
+	}
+
+	private void checkNotForSale(PlanetStatus planetStatus) {
+		if (planetStatus.equals(PlanetStatus.FORSALE)) {
+			throw new AlreadyOnSaleException();
+		}
+	}
+
+	private void checkBalance(Integer balance, Integer price) {
+		if (balance < price) {
+			throw new NotEnoughBalanceException();
 		}
 	}
 }
