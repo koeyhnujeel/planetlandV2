eb6224481e50ee9d928ef4e6222da6bec5434902
zunza <fksfks015@naver.com>
2024-03-05 17:45:03 +0900

    :sparkles: TransactionManager 생성(거래 내역 관련 생성, 행성 삭제, 조회 역할 담당)

--- src/main/java/com/planetlandV2/Manager/TransactionManager.java
+++ src/main/java/com/planetlandV2/Manager/TransactionManager.java
@@ -1,9 +1,17 @@
 package com.planetlandV2.Manager;
 
+import java.util.List;
+
 import com.planetlandV2.domain.Planet;
 import com.planetlandV2.domain.User;
+import com.planetlandV2.request.TransactionPage;
+import com.planetlandV2.response.TransactionResponse;
 
 public interface TransactionManager {
 
 	void create(User buyer, User seller, Planet planet);
+
+	void removePlanet(Planet planet);
+
+	List<TransactionResponse> readList(Long userId, TransactionPage transactionPage);
 }
--- src/main/java/com/planetlandV2/Manager/TransactionManagerImpl.java
+++ src/main/java/com/planetlandV2/Manager/TransactionManagerImpl.java
@@ -1,11 +1,17 @@
 package com.planetlandV2.Manager;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.planetlandV2.domain.Planet;
 import com.planetlandV2.domain.Transaction;
 import com.planetlandV2.domain.User;
 import com.planetlandV2.repository.TransactionRepository;
+import com.planetlandV2.request.TransactionPage;
+import com.planetlandV2.response.TransactionResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,5 +31,19 @@ public class TransactionManagerImpl implements TransactionManager {
 			.savedPlanetName(planet.getPlanetName())
 			.build()
 		);
+	}
+
+	@Override
+	public List<TransactionResponse> readList(Long userId, TransactionPage transactionPage) {
+		List<Transaction> transactionList = transactionRepository.getTransactionList(userId, transactionPage);
+		return transactionList.stream()
+			.map(Transaction::toResponse)
+			.collect(Collectors.toList());
+	}
+
+	@Override
+	public void removePlanet(Planet planet) {
+		Optional<List<Transaction>> transactionList = transactionRepository.findByPlanet(planet);
+		transactionList.ifPresent(transactions -> transactions.forEach(Transaction::deletePlanet));
 	}
 }
