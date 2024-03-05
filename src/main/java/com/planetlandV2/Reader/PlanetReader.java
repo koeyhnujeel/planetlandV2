998b214b3d17681e819b67bc7facfc158a981ee0
zunza <fksfks015@naver.com>
2024-03-05 17:46:04 +0900

    :sparkles: PlanetReader 생성(Planet 관련 조회 역할 담당)

--- src/main/java/com/planetlandV2/Reader/PlanetReader.java
+++ src/main/java/com/planetlandV2/Reader/PlanetReader.java
@@ -1,10 +1,16 @@
 package com.planetlandV2.Reader;
 
+import java.util.List;
+
 import com.planetlandV2.domain.Planet;
+import com.planetlandV2.request.PlanetPage;
+import com.planetlandV2.response.PlanetResponse;
 
 public interface PlanetReader {
 
 	Planet read(Long planetId);
+
+	List<PlanetResponse> readList(PlanetPage planetPage);
 
 	Planet readForBuy(Long planetId);
 }
--- src/main/java/com/planetlandV2/Reader/PlanetReaderImpl.java
+++ src/main/java/com/planetlandV2/Reader/PlanetReaderImpl.java
@@ -1,10 +1,15 @@
 package com.planetlandV2.Reader;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.planetlandV2.domain.Planet;
 import com.planetlandV2.exception.planet.PlanetNotFound;
 import com.planetlandV2.repository.PlanetRepository;
+import com.planetlandV2.request.PlanetPage;
+import com.planetlandV2.response.PlanetResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +23,13 @@ public class PlanetReaderImpl implements PlanetReader {
 	public Planet read(Long planetId) {
 		return planetRepository.findById(planetId)
 			.orElseThrow(PlanetNotFound::new);
+	}
+
+	@Override
+	public List<PlanetResponse> readList(PlanetPage planetPage) {
+		return planetRepository.getList(planetPage).stream()
+			.map(Planet::toResponse)
+			.collect(Collectors.toList());
 	}
 
 	@Override
